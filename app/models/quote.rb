03cc71f4975746198b06ae62a7f7dd9c0b2a3766
Sandra Harrasser <design@sandraharrasser.com>
2015-11-26 22:06:53 +0000

    added initials and test

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,16 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => { :maximum => 200, :minimum => 3}
 	validates :author, :presence => true, :length => { :maximum => 50, :minimum => 3}
+
+	def unique_tag
+		abbr = []
+		self.author.split(" ").collect do |sub_string|
+			abbr << sub_string[0]
+	end
+	abbr.join + '#' + self.id.to_s
+	end
+
+
+
+
 end
--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class QuoteTest < ActiveSupport::TestCase
-   test "the truth" do
-     assert true
+   test "unique_tag" do 
+   	quote = Quote.create(:author => 'Sandra Harrasser', :saying => 'Life IS and adventure, its the journey that matters.')
+   	expected = 'SH#' + quote.id.to_s
+   	actual = quote.unique_tag
+   	assert_equal expected, actual
    end
+
+
 end
